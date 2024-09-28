d42b4d1c5dc3bf908435e2534614fc0498149ebc
Phubate Nimnuan <111554614+Plashon@users.noreply.github.com>
2024-09-29 02:28:09 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 const role = require("./models/role.model")
 
 const corsOptions = {
-    origin: process.env.corsConnect,
+    origin: "https://midterm65-1cef5z18h-plashons-projects.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     optionSuccessStatus:200,
