12a27fc87cab1ec59976ae34d77d54dca279cde3
Phubate Nimnuan <111554614+Plashon@users.noreply.github.com>
2024-09-29 18:06:31 +0700

    Update Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="container flex flex-col items-center mx-auto space-y-4 mb-5">
       <div className="font-bold text-4xl text-center mt-5">
-        <a href="http://localhost:5173/">
+        <a href="/">
           <span className="text-blue-700">Book</span> Bookshelf
         </a>
       </div>
